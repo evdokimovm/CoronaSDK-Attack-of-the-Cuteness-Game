fe1b0f837ecb61686fb6a8315a2b74ef3dc72e02
evdokimovm <tel89147925532@gmail.com>
2015-11-19 19:57:59 +1000

    Graphics and audio assets. Config, build settings and basic main lua

---
+++ main.lua
@@ -0,0 +1,42 @@
+-- Project: Attack of the Cuteness Game
+-- http://MasteringCoronaSDK.com
+-- Version: 1.0
+-- Copyright 2013 J. A. Whye. All Rights Reserved.
+-- "Space Cute" art by Daniel Cook (Lostgarden.com) 
+
+-- housekeeping stuff
+
+display.setStatusBar(display.HiddenStatusBar)
+
+local centerX = display.contentCenterX
+local centerY = display.contentCenterY
+
+-- set up forward references
+
+-- preload audio
+
+-- create play screen
+
+-- game functions
+
+local function spawnEnemy()
+
+end
+
+local function startGame()
+
+end
+
+local function planetDamage()
+
+end
+
+local function hitPlanet(obj)
+
+end
+
+local function shipSmash(event)
+
+end
+
+startGame()
